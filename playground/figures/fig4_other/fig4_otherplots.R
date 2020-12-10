b8c9263bc1399dd1fc2d4a1a9f7f7430e5b00cc7
VladaMilch <milchv@gmail.com>
2020-12-10 17:47:21 +0000

    figure 4 A

--- playground/figures/fig4_other/fig4_otherplots.R
+++ playground/figures/fig4_other/fig4_otherplots.R
@@ -60,13 +60,15 @@ calc_data4fig4 <- function(
 }
   
   
-fe70 <- lapply(X = c(1, 0.5, 0.25, 0.125, 0.0625), FUN = function(genoP){
+fe70 <- lapply(X = c(1, 0.5, 0.25, 0.125, 0.0625)[1:2], FUN = function(genoP){
   calc_data4fig4(
-    req_pups = req_pups_1000,
+    req_pups = c(10,20,50,100),
     genotype_probability = genoP, 
     confiP = 0.9, 
     effertyP = 0.7, litmean = 7)
 })
+
+save(fe70, file = "fe70.Rdata")
 
 fe100 <- lapply(X = c(1, 0.5, 0.25, 0.125, 0.0625), FUN = function(genoP){
     calc_data4fig4(
@@ -78,18 +80,59 @@ fe100 <- lapply(X = c(1, 0.5, 0.25, 0.125, 0.0625), FUN = function(genoP){
 
 
 
-required_F70 %>%
+fe70[[1]] %>%
   ggplot(aes(x=req_pups)) + 
   #geom_point(aes(y=req_br_festing50, col = "Festing 50%"), colour = "red", alpha=0.5, size=0.5) + 
   #geom_line(aes(y=req_br_festing50, col = "Festing 50%"), alpha=0.3, size=0.3) + 
   #geom_point(aes(y=req_br_festing90, col = "Festing 90%")) +
-  geom_point(aes(y=req_br_festing95, col = "Festing 95%")) + 
-  geom_point(aes(y=req_br_poisson95, col = "Poisson 95%")) + 
-  geom_point(aes(y=req_mendel, col = "Mendel ( = Poisson 50% )")) + 
+  geom_point(aes(y= req_poisson, col = "poisson")) + 
+  geom_point(aes(y=req_festing, col = "festing")) + 
+  geom_point(aes(y=req_mendel, col = "mendel")) + 
   labs(x="Required number of pups", 
        y = "Smallest number of breedings", 
        caption = "Mean litter size: 7,  Effective fertility: 70%", 
        title = "Model comparison for the smallest required number of breegins", colour = "Method") + 
   theme(legend.position="right") 
 
-save(required_F70, file = "required_fertility70.RData")
+xx = fe70[[1]]
+surplus_pois <- round(100*(xx$req_poisson-xx$req_mendel)/xx$req_mendel)
+surplus_fest <- round(100*(xx$req_festing-xx$req_mendel)/xx$req_mendel)
+yy <- data.frame(xx, surplus_pois, surplus_fest)
+
+barplot(yy$surplus_fest, yy$surplus_pois)
+
+library(lattice)
+barchart(Species~Reason,data=Reasonstats,groups=Catergory, 
+         scales=list(x=list(rot=90,cex=0.8)))
+
+
+Reasonstats <- read.table(text="Category         Reason  Species
+                                 Decline        Genuine       24
+                                Improved        Genuine       16
+                                Improved  Misclassified       85
+                                 Decline  Misclassified       41
+                                 Decline      Taxonomic        2
+                                Improved      Taxonomic        7
+                                 Decline        Unclear       41
+                                Improved        Unclear      117", header=T)
+
+ReasonstatsDec <- Reasonstats[which(Reasonstats$Category=="Decline"),]
+ReasonstatsImp <- Reasonstats[which(Reasonstats$Category=="Improved"),]
+Reasonstats3   <- cbind(ReasonstatsImp[,3], ReasonstatsDec[,3])
+colnames(Reasonstats3) <- c("Improved", "Decline")
+rownames(Reasonstats3) <- ReasonstatsImp$Reason
+
+#windows()
+barplot(t(Reasonstats3), beside=TRUE, ylab="number of species", 
+        cex.names=0.8, las=2, ylim=c(0,120), col=c("darkblue","red"))
+
+zz <- t(yy)
+colnames(zz) <- yy$req_pups
+barplot(zz[c(4,2,3),], beside = TRUE, 
+        ylab="required number of of breedings", 
+        xlab = "number of pups required")
+
+barplot(zz[c(5,6),], beside = TRUE, 
+        ylab="surplus % breedings", 
+        xlab = "number of pups required")
+
